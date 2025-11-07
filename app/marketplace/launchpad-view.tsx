5f585ce3ba04f1a972673e2f20e67d4903eb7ccf
Lironie <tremblay.julien.pro@gmail.com>
2025-11-07 13:15:18 -0500

    updated grid

--- app/marketplace/launchpad-view.tsx
+++ app/marketplace/launchpad-view.tsx
@@ -622,7 +622,7 @@ function LaunchpadViewContent({ onPayDeposit }: LaunchpadViewProps) {
 
         {/* List */}
         {isLoading ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {Array.from({ length: 6 }).map((_, i) => (
               <Card
                 key={i}
