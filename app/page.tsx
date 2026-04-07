ba36bb3adcd38c7abe61ddb59b6c7ac9f4930ce6
Lironie <tremblay.julien.pro@gmail.com>
2026-04-07 13:12:35 -0400

    Remove homepage launchpad SSR prefetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, dehydrate } from "@tanstack/react-query";
 import { getCachedHeadlineStats } from "@/lib/server/headline-stats";
 import { getEthPriceInUSD } from "@/utils/getEthPriceInUSD";
-import { prefetchDashboardLaunchpadData } from "@/lib/server/dashboard-launchpad-prefetch";
 import { prefetchHomeProtocolMetricsData } from "@/lib/server/home-protocol-metrics-prefetch";
 import { PageWrapper } from "./components/page-wrapper";
 import { HydrationWrapper } from "./components/hydration-wrapper";
@@ -34,7 +33,6 @@ export default async function HomePage() {
       queryFn: getEthPriceInUSD,
       staleTime: 60_000,
     }),
-    prefetchDashboardLaunchpadData(queryClient),
   ]);
 
   const headlineStats = queryClient.getQueryData<
