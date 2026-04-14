efa9eef36b3b12bd926186daf2c977437a702ff4
Lironie <tremblay.julien.pro@gmail.com>
2026-04-14 16:10:55 -0400

    Add Irene to KoL wallet list

--- lib/kol.ts
+++ lib/kol.ts
@@ -4,10 +4,11 @@
 
 /** Approved KoL wallet addresses (lowercased). */
 export const KOL_WALLETS: readonly string[] = [
-  "0xd7f58cfaa74dd2de35b44833ee20be7dab8ecec9",
-  "0x8f2c0f09f5135312772145a49e633eac05cfbb93",
+  "0xd7f58cfaa74dd2de35b44833ee20be7dab8ecec9", // Eungo
+  "0x8f2c0f09f5135312772145a49e633eac05cfbb93", // Joshiker
   "0x4fcc122cdea3c5d6c56f900800adec75cb15b81a",
-  "0x913dd1468d8b7ed4c52d4eb41d94b5ff61d0dae6",
+  "0x913dd1468d8b7ed4c52d4eb41d94b5ff61d0dae6", // Jazz
+  "0xd5199714f0bf37e5b163e287ffc83e896890ff4a", // Irene
 ] as const;
 
 const KOL_SET = new Set(KOL_WALLETS);
